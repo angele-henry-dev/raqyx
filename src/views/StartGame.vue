757d47bee0bf01b3e9b2b38652305cd397ac0059
Angele <angele.henry@outlook.com>
2023-05-17 17:06:00 +0200

    Game area

--- src/views/StartGame.vue
+++ src/views/StartGame.vue
@@ -1,11 +1,44 @@
 <template>
   <ion-page>
     <ion-content>
-      <ion-title size="large">New game</ion-title>
+      <ion-grid>
+        <ion-row class="ion-justify-content-between">
+          <ion-col size="5" class="ion-text-start">
+            650798
+          </ion-col>
+          <ion-col size="5" class="ion-text-end">
+            0% / 75%
+          </ion-col>
+        </ion-row>
+        <ion-row class="game-area">
+          <game-area />
+        </ion-row>
+        <ion-row class="ion-justify-content-between">
+          <ion-col size="5" class="ion-text-start">
+            Level 1
+          </ion-col>
+          <ion-col size="5" class="ion-text-end">
+            o x1
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-  import { IonPage, IonTitle, IonContent } from '@ionic/vue';
+  import { IonPage, IonContent } from '@ionic/vue';
+  import GameArea from '@/components/GameArea.vue';
 </script>
+
+<style scoped>
+  ion-grid {
+    width: 90%;
+    height: 100%;
+    margin: auto;
+  }
+  .game-area {
+    height: 90%;
+    max-height: 600px;
+  }
+</style>
