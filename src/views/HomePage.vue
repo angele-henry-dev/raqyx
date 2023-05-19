e95954c66a7c08a2255beed2e44f168d696b6aa3
Angele <angele.henry@outlook.com>
2023-05-19 12:56:30 +0200

    First animation

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,18 +4,13 @@
       <ion-button router-link="/newgame">New game</ion-button>
       <ion-button router-link="/continuegame">Continue</ion-button>
       <ion-img
-          src="/assets/images/wp5289804.jpg"
+          src="/assets/images/titlescreen.jpg"
           alt="Homepage image of the game"
       ></ion-img>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-  import { IonImg, IonButton } from '@ionic/vue';
-  import { defineComponent } from 'vue';
-
-  export default defineComponent({
-    components: { IonImg, IonButton },
-  });
+<script setup lang="ts">
+  import { IonPage, IonContent, IonImg, IonButton } from '@ionic/vue';
 </script>
--- src/views/StartGame.vue
+++ src/views/StartGame.vue
@@ -10,8 +10,11 @@
             0% / 75%
           </ion-col>
         </ion-row>
-        <ion-row class="game-area">
-          <game-area />
+        <ion-row id="game-area">
+          <div ref="player" id="player"></div>
+          <div id="container">
+            <div id="ennemies"></div>
+          </div>
         </ion-row>
         <ion-row class="ion-justify-content-between">
           <ion-col size="5" class="ion-text-start">
@@ -27,18 +30,81 @@
 </template>
 
 <script setup lang="ts">
-  import { IonPage, IonContent } from '@ionic/vue';
-  import GameArea from '@/components/GameArea.vue';
+  import { onMounted, ref } from 'vue'
+  import { 
+    IonPage,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    onIonViewWillEnter,
+    onIonViewDidEnter,
+    onIonViewWillLeave,
+    onIonViewDidLeave,
+    createAnimation,
+  } from '@ionic/vue'
+
+  // Variables
+  const animation = createAnimation()
+  const player = ref<HTMLDivElement | null>(null)
+
+  // Methods
+  function launch() {
+    //
+  }
+  
+  // Setup
+  onMounted(() => {
+    if (player.value) {
+      console.log(player.value)
+      animation
+        .addElement(player.value)
+        .duration(1500)
+        .iterations(Infinity)
+        .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
+      animation.play()
+    }
+  })
+
+  onIonViewDidEnter(() => {
+    console.log('Page did enter')
+  })
+
+  onIonViewDidLeave(() => {
+    console.log('Page did leave')
+  })
+
+  onIonViewWillEnter(() => {
+    console.log('Page will enter');
+  })
+
+  onIonViewWillLeave(() => {
+    console.log('Page will leave');
+  })
 </script>
 
 <style scoped>
   ion-grid {
     width: 90%;
-    height: 100%;
+    height: 95%;
     margin: auto;
   }
-  .game-area {
+  #game-area {
     height: 90%;
     max-height: 600px;
+  }
+  #container {
+    width: 100%;
+    height: 100%;
+    background-color: var(--ion-color-primary);
+    margin: 2px;
+    padding: 0;
+  }
+  #player {
+    width: 5px;
+    height: 5px;
+    border-radius: 100%;
+    background-color: var(--ion-color-secondary);
+    position: absolute;
   }
 </style>
