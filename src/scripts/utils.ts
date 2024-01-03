2b89655615753f7bd66ebb5e6be33154c672ee13
Angèle Henry <angele.henry@outlook.com>
2024-01-03 18:26:44 +0100

    Remove obsolete

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,9 +1,3 @@
 export const randomIntFromInterval = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
 };
-
-export const isBetween = function(point: number, a: number, b: number) {
-    const min = Math.min.apply(Math, [a, b]);
-    const max = Math.max.apply(Math, [a, b]);
-    return point > min && point < max;
-};
