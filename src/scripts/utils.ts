9e85b97cd64e8b593f95781ee2a75e40e4d4ab5c
Angèle Henry <angele.henry@outlook.com>
2024-01-07 17:58:23 +0100

    Add comments

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,3 +1,10 @@
-export const randomIntFromInterval = (min: number, max: number) => {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+/**
+ * Generates a random integer within a specified range.
+ * @param min The minimum value of the range (inclusive).
+ * @param max The maximum value of the range (inclusive).
+ * @returns A random integer within the specified range.
+ */
+export const randomIntFromInterval = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
