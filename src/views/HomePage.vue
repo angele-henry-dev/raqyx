28bbc0682e34603bdd7c6a784b017e4ee0d47730
Angèle Henry <angele.henry@outlook.com>
2024-01-12 16:31:06 +0100

    Icons

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -20,21 +20,31 @@
         <ion-row class="ion-justify-content-center">
           <ion-col size="6" class="menu">
             <div>
-              <ion-button class="broken-letter" expand="block" fill="clear" size="large" @click="newGame()">New game</ion-button>
-              <ion-button class="deactivated" expand="block" fill="clear" size="large" @click="loadGame()">Continue</ion-button>
-              <ion-button expand="block"  fill="clear" id="open-modal-stats">Stats</ion-button>
+              <ion-button class="menu-btn broken-letter" expand="block" fill="clear" size="large" @click="newGame()">New game</ion-button>
+              <ion-button class="menu-btn deactivated" expand="block" fill="clear" size="large" @click="loadGame()">Continue</ion-button>
+              <ion-button class="menu-btn" expand="block"  fill="clear" id="open-modal-stats">Stats</ion-button>
             </div>
           </ion-col>
         </ion-row>
       </ion-grid>
     </ion-content>
-    <ion-footer>
-      <ion-row class="ion-justify-content-between">
-        <ion-col size="3">
-          <ion-button fill="clear" id="open-modal-settings">Settings</ion-button>
+    <ion-footer class="settings_icon">
+      <ion-row>
+        <ion-col>
+          <ion-button fill="clear" id="open-modal-settings">
+            <ion-img
+              src="/assets/images/icon-settings.png"
+              alt="Settings"
+            ></ion-img>
+          </ion-button>
         </ion-col>
-        <ion-col size="3" class="ion-text-end">
-          <ion-button expand="block" fill="clear" id="open-modal-rules">Rules</ion-button>
+        <ion-col class="ion-text-end">
+          <ion-button fill="clear" id="open-modal-rules">
+            <ion-img
+              src="/assets/images/icon-rules.png"
+              alt="Settings"
+            ></ion-img>
+          </ion-button>
         </ion-col>
       </ion-row>
     </ion-footer>
