b8cdd8591b16cd17d546f2df8f1e135e6b0c06e2
Angèle Henry <angele.henry@outlook.com>
2024-02-25 15:28:20 +0100

    Events

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,30 +1,28 @@
 <template>
-    <ion-header class="events">
-      <ion-row class="ion-text-center">
-        <ion-col>
-          Here the events
-        </ion-col>
-      </ion-row>
-    </ion-header>
+  <div class="shooting-stars">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
     <ion-content class="homepage">
-      <div class="shooting-stars">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
       <ion-row>
         <ion-img
             src="/assets/images/raqyx_title_green.png"
             alt="Game title"
         ></ion-img>
       </ion-row>
+      <ion-row class="events ion-text-center">
+        <ion-col>
+          No events today
+        </ion-col>
+      </ion-row>
     </ion-content>
     <RulesModal />
 </template>
 
 <script setup lang="ts">
-  import { IonContent, IonHeader, IonRow, IonCol, IonImg } from '@ionic/vue';
+  import { IonContent, IonRow, IonCol, IonImg } from '@ionic/vue';
   import RulesModal from '@/views/RulesModal.vue';
 </script>
