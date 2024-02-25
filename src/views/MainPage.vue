6b8ada2e7dbbd51d13bfc0d5c7ed483e5e2e62bb
Angèle Henry <angele.henry@outlook.com>
2024-02-25 15:15:24 +0100

    Design menu

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -10,36 +10,48 @@
                     <source src="/assets/music/theme 1.10.wav" autoplay :loop="true" />
                 </audio>
                 <ul>
-                    <li :class="currentTab == 'GamePage' ? 'ingame' : ''"><ion-button v-if="currentTab == 'HomePage'" class="broken-letter" fill="clear" @click="currentTab = 'GamePage'">
-                        <ion-img
-                            src="/assets/images/icon-game.png"
-                            alt="Play"
-                        ></ion-img>
-                    </ion-button>
-                    <ion-button v-if="currentTab == 'GamePage'" fill="clear" @click="currentTab = 'HomePage'">
-                        <ion-img
-                            src="/assets/images/icon-home.png"
-                            alt="Home"
-                        ></ion-img>
-                    </ion-button></li>
-                    <li :class="currentTab == 'GamePage' ? 'ingame' : ''"><ion-button fill="clear" id="open-modal-settings">
-                        <ion-img
-                            src="/assets/images/icon-settings.png"
-                            alt="Settings"
-                        ></ion-img>
-                    </ion-button></li>
-                    <li :class="currentTab == 'GamePage' ? 'ingame' : ''"><ion-button fill="clear" id="open-modal-stats">
-                        <ion-img
-                        src="/assets/images/icon-stats.png"
-                        alt="Stats"
-                        ></ion-img>
-                    </ion-button></li>
-                    <li :class="currentTab == 'GamePage' ? 'ingame' : ''"><ion-button fill="clear" id="open-modal-rules">
-                        <ion-img
-                        src="/assets/images/icon-rules.png"
-                        alt="Rules"
-                        ></ion-img>
-                    </ion-button></li>
+                    <li :class="currentTab == 'GamePage' ? 'ingame' : ''">
+                        <ion-button v-if="currentTab == 'HomePage'" class="broken-letter" fill="clear" @click="currentTab = 'GamePage'">
+                            <ion-img
+                                src="/assets/images/icon-game.png"
+                                alt="Play"
+                            ></ion-img>
+                            <span v-if="currentTab == 'HomePage'">Start</span>
+                        </ion-button>
+                        <ion-button v-if="currentTab == 'GamePage'" fill="clear" @click="currentTab = 'HomePage'">
+                            <ion-img
+                                src="/assets/images/icon-home.png"
+                                alt="Home"
+                            ></ion-img>
+                        </ion-button>
+                    </li>
+                    <li :class="currentTab == 'GamePage' ? 'ingame' : ''">
+                        <ion-button fill="clear" id="open-modal-settings">
+                            <ion-img
+                                src="/assets/images/icon-settings.png"
+                                alt="Settings"
+                            ></ion-img>
+                            <span v-if="currentTab == 'HomePage'">Settings</span>
+                        </ion-button>
+                    </li>
+                    <li :class="currentTab == 'GamePage' ? 'ingame' : ''">
+                        <ion-button fill="clear" id="open-modal-stats">
+                            <ion-img
+                            src="/assets/images/icon-stats.png"
+                            alt="Stats"
+                            ></ion-img>
+                            <span v-if="currentTab == 'HomePage'">Stats</span>
+                        </ion-button>
+                    </li>
+                    <li :class="currentTab == 'GamePage' ? 'ingame' : ''">
+                        <ion-button fill="clear" id="open-modal-rules">
+                            <ion-img
+                            src="/assets/images/icon-rules.png"
+                            alt="Rules"
+                            ></ion-img>
+                            <span v-if="currentTab == 'HomePage'">Rules</span>
+                        </ion-button>
+                    </li>
                 </ul>
                 <!-- <ion-button fill="clear" @click="play()">Plop</ion-button> -->
             </ion-row>
